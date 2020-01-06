6bc880e6bf596aace09a3ec3900476a413fb058e
2b0f7bbd <est@localhost.localdomain>
2020-01-06 22:41:09 +0500

    Updates

---
+++ precache-manifest.c30a3434e76d9fcbfe0bc82ddfc34bc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cab121557f299c4656ae3e0c3f9a36d",
+    "url": "/todo_react_app/index.html"
+  },
+  {
+    "revision": "02b52074291df6fc29a6",
+    "url": "/todo_react_app/static/css/main.0eeeefa0.chunk.css"
+  },
+  {
+    "revision": "c6b658de39f43b2aebb5",
+    "url": "/todo_react_app/static/js/2.6221310f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo_react_app/static/js/2.6221310f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02b52074291df6fc29a6",
+    "url": "/todo_react_app/static/js/main.a3e59b88.chunk.js"
+  },
+  {
+    "revision": "c154a8e484ce3f2311ed",
+    "url": "/todo_react_app/static/js/runtime-main.909588c5.js"
+  }
+]);
